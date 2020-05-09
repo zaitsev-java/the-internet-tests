0f7361bc439a8335d325999b92a69f8111db695f
alex <zaitsev.java@gmail.com>
2020-05-09 17:54:39 -0400

    NegativeLoginTests transformed to use DataProvider

--- src/test/java/com/herokuapp/theinternet/NegativeLoginTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeLoginTests.java
@@ -3,22 +3,27 @@ package com.herokuapp.theinternet;
 import com.herokuapp.theinternet.pages.HomePage;
 import com.herokuapp.theinternet.pages.LoginPage;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.util.Map;
 
 public class NegativeLoginTests extends TestUtilities {
 
-    @Parameters({"username", "password", "expectedMessage"})
-    @Test
-    public void testLoginNegative(String username, String password, String expectedErrorMessage) {
-        log.info("Starting testLoginNegative test");
+
+    @Test(dataProvider = "csvReader", dataProviderClass = CSVDataProviders.class)
+    public void testLoginNegative(Map<String, String> testData) {
+        String no = testData.get("no");
+        String username = testData.get("username");
+        String password = testData.get("password");
+        String expectedMessage = testData.get("expectedMessage");
+        log.info("Starting testLoginNegative test # " + no);
         HomePage homePage = new HomePage(driver, log);
         homePage.open();
         LoginPage loginPage = homePage.clickLoginLink();
         loginPage.logInNegative(username, password);
         loginPage.waitForErrorMessage();
         String message = loginPage.getErrorMessageText();
-        Assert.assertTrue(message.contains(expectedErrorMessage));
+        Assert.assertTrue(message.contains(expectedMessage));
 
     }
 }
