64ad5fc87457f945866053e8a1e5e36bad38c6d2
alex <zaitsev.java@gmail.com>
2020-05-09 21:08:25 -0400

    TestListener class added to the project

--- src/main/java/com/herokuapp/theinternet/TestBase.java
+++ src/main/java/com/herokuapp/theinternet/TestBase.java
@@ -4,14 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
 
+@Listeners({ TestListener.class })
 public class TestBase {
 
     protected WebDriver driver;
